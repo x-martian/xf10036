2803d3a1b64f141768f393068bc49755ffb0487b
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-11-17 17:02:26 +0000

    Add Code to Handle Events

--- playground/wpfpractice/ExpenseIt/HomePage.xaml.cs
+++ playground/wpfpractice/ExpenseIt/HomePage.xaml.cs
@@ -10,5 +10,11 @@ namespace ExpenseIt
         {
             InitializeComponent();
         }
+        private void viewButton_Click(object sender, RoutedEventArgs args)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage();
+            this.NavigationService.Navigate(expenseReportPage);
+        }
     }
 }

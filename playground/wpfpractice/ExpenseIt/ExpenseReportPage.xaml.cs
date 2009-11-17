a8bc61daa9ce33632193b33815a30ab756d098d5
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-11-17 16:47:51 +0000

    basic

---
+++ playground/wpfpractice/ExpenseIt/ExpenseReportPage.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+namespace ExpenseIt
+{
+    public partial class ExpenseReportPage : Page
+    {
+        public ExpenseReportPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ playground/wpfpractice/ExpenseIt/HomePage.xaml.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+namespace ExpenseIt
+{
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}

c7825ffefa0cec27e92d54cdf0578ee998eaf0bd
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-11-19 00:12:21 +0000

    create the contract and views

---
+++ trunk/playground/dotNet/CalculatorV1/Calc1AddInView/ICalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn.Pipeline;
+
+namespace CalcAddInViews
+{
+    // The AddInBaseAttribute identifes this interface as the basis for
+    // the add-in view pipeline segment.
+    [AddInBase()]
+    public interface ICalculator
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}

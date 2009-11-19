4bccf46feb2b0ccbe9444d143febe1b5a9aee52a
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-11-19 00:22:51 +0000

    Creating the Add-in-side Adapter

---
+++ trunk/playground/dotNet/CalculatorV1/Calc1AddInSideAdapter/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System.AddIn.Pipeline;
+using CalcAddInViews;
+using CalculatorContracts;
+
+namespace CalcAddInSideAdapters
+{
+    // The AddInAdapterAttribute identifes this class as the add-in-side adapter
+    // pipeline segment.
+    [AddInAdapter()]
+    public class CalculatorViewToContractAddInSideAdapter :
+        ContractBase, ICalc1Contract
+    {
+        private ICalculator _view;
+
+        public CalculatorViewToContractAddInSideAdapter(ICalculator view)
+        {
+            _view = view;
+        }
+
+        public virtual double Add(double a, double b)
+        {
+            return _view.Add(a, b);
+        }
+
+        public virtual double Subtract(double a, double b)
+        {
+            return _view.Subtract(a, b);
+        }
+
+        public virtual double Multiply(double a, double b)
+        {
+            return _view.Multiply(a, b);
+        }
+
+        public virtual double Divide(double a, double b)
+        {
+            return _view.Divide(a, b);
+        }
+    }
+}

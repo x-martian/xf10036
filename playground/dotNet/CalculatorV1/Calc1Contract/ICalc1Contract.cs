a433be649556a257d5b4b8b3de8e184729ff363c
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-11-19 00:12:21 +0000

    create the contract and views

---
+++ playground/dotNet/CalculatorV1/Calc1Contract/ICalc1Contract.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.AddIn.Contract;
+using System.AddIn.Pipeline;
+
+namespace CalculatorContracts
+{
+    // The AddInContractAttribute identifes this pipeline segment as a 
+    // contract.
+    [AddInContract]
+    public interface ICalc1Contract : IContract
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+}

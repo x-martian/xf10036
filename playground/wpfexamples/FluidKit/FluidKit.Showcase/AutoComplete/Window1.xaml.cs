a4ed2231d0bc32c117f9075d9ca63947296d53c0
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-12-03 21:00:40 +0000

    Dec 03, 2009

---
+++ playground/wpfexamples/FluidKit/FluidKit.Showcase/AutoComplete/Window1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FluidKit.Showcase;
+
+namespace AutoComplete
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+
+			Loaded += (s, args) =>
+			          	{
+			          		var source = FindResource("DataSource") as StringCollection;
+			          		for (int i = 0; i < 30; i++)
+			          		{
+			          			source.Add("Item - " + i);
+			          		}
+			          	};
+		}
+	}
+}
---
+++ playground/wpfexamples/FluidKit/FluidKit.Showcase/View3D/Window1.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FluidKit.Controls.View3D;
+
+namespace FluidKit.Showcase.View3D
+{
+	/// <summary>
+	/// Interaction logic for Window1.xaml
+	/// </summary>
+	public partial class Window1 : Window
+	{
+		public Window1()
+		{
+			InitializeComponent();
+		}
+
+		private void AddModel(object sender, RoutedEventArgs e)
+		{
+			Button b = sender as Button;
+			MapItemVisual3D model = null;
+			switch (b.Name)
+			{
+				case "_cube":
+					model = new Cube();
+					//model.FaceBrush = new SolidColorBrush(Colors.LightCoral);
+					break;
+				case "_cylinder":
+					model = new Cylinder();
+					//model.FaceBrush = new SolidColorBrush(Colors.Ivory);
+					break;
+				case "_sphere":
+					model = new Sphere();
+					//model.FaceBrush = new LinearGradientBrush(Colors.LightBlue, Colors.Orchid, 90);
+					break;
+				case "_torus":
+					model = new Torus();
+					//model.FaceBrush = new SolidColorBrush(Colors.Khaki);
+					break;
+				case "_cone":
+					model = new Cone();
+					//model.FaceBrush = new LinearGradientBrush(Colors.LightBlue, Colors.Orchid, 90);
+					break;
+			}
+			model.FaceBrush = new LinearGradientBrush(Colors.LightBlue, Colors.Orchid, 90);
+			model.FaceBrush.Opacity = 0.5;
+			model.EdgePen = new Pen(Brushes.Black, 1);
+
+			_view3D.Children.Add(model);
+
+		}
+
+
+	}
+}

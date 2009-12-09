a8f1b54362e9375946a6ee1e3609c5a48c0ec0e7
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-12-09 22:11:53 +0000

    In this version, if the button has the input focus when "File->Close" is selected, both "Open" and "Close" under "File" will no longer available.

---
+++ playground/wpfpractice/TaskSwitching/Window1.xaml.vb
@@ -0,0 +1,35 @@
+﻿Class Window1 
+
+    Private Sub MenuItem1_Click(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MenuItem1.Click
+        Close()
+    End Sub
+
+    Private Sub FileClose_CanExcute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        Debug.WriteLine("Window CanExcute")
+        e.CanExecute = True
+        e.Handled = True
+    End Sub
+
+    Private Sub FileClose_Excuted(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
+        userControl1.Visibility = Windows.Visibility.Hidden
+        Me.dockPanel.Children.Remove(userControl1)
+        Debug.WriteLine("Window Executed")
+        Dim focused As Boolean = Menu1.Focus()
+        Me.UpdateLayout()
+    End Sub
+
+    Private Sub FileOpen_CanExcute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        e.CanExecute = True
+    End Sub
+
+    Private Sub FileOpen_Excuted(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
+    End Sub
+
+    Private Sub UserControl_CanExecute(ByVal sender As System.Object, ByVal e As System.Windows.Input.CanExecuteRoutedEventArgs)
+        Debug.WriteLine("UserControl CanExecute")
+    End Sub
+
+    Private Sub UserControl_Executed(ByVal sender As System.Object, ByVal e As System.Windows.Input.ExecutedRoutedEventArgs)
+        Debug.WriteLine("UserControl Executed")
+    End Sub
+End Class

b38461bfe2ebfc6f23f9121630e152c349621e11
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-12-22 20:25:20 +0000

    reproduced "This Visual is not connected to a PresentationSource." exception in PointToScreen function

--- trunk/playground/wpfpractice/TaskSwitching/UserControl1.xaml.vb
+++ trunk/playground/wpfpractice/TaskSwitching/UserControl1.xaml.vb
@@ -1,3 +1,11 @@
 ﻿Partial Public Class UserControl1
+    Private Sub window1_LocationChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim pt As Point = New Point(0.0, 0.0)
+        pt = Me.PointToScreen(pt)
+        TextBox1.Text = pt.ToString()
+    End Sub
 
+    Private Sub UserControl1_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
+        AddHandler Window.GetWindow(Me).LocationChanged, AddressOf window1_LocationChanged
+    End Sub
 End Class

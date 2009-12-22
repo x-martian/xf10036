237fe7ec8fe25cfda0da8ecc3534da6ab6dc96b0
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-12-22 20:58:12 +0000

    (partially) resolved the issue demonstrated in rev 89

--- trunk/playground/wpfpractice/TaskSwitching/UserControl1.xaml.vb
+++ trunk/playground/wpfpractice/TaskSwitching/UserControl1.xaml.vb
@@ -8,4 +8,11 @@
     Private Sub UserControl1_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
         AddHandler Window.GetWindow(Me).LocationChanged, AddressOf window1_LocationChanged
     End Sub
+
+    Private Sub UserControl1_IsVisibleChanged(ByVal sender As System.Object, ByVal e As System.Windows.DependencyPropertyChangedEventArgs) Handles MyBase.IsVisibleChanged
+        If e.OldValue = False And e.NewValue = True Then
+        ElseIf e.OldValue = True And e.NewValue = False Then
+            RemoveHandler Window.GetWindow(Me).LocationChanged, AddressOf window1_LocationChanged
+        End If
+    End Sub
 End Class

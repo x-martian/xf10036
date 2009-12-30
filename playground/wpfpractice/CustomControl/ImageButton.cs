bd774e76be13413a21ccba5b08e9d35ef294e1d8
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-12-30 01:18:58 +0000

    Apply descriptions for your custom property

--- playground/wpfpractice/CustomControl/ImageButton.cs
+++ playground/wpfpractice/CustomControl/ImageButton.cs
@@ -9,14 +9,17 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.ComponentModel;
 
 namespace CustomControl
 {
+	[Description("Represents a custom button control that responds to a Click event.  Displays an image using a custom Source property if the Source property is bound to an Image in the template.")]
 	public class ImageButton : Button
 	{
 		public ImageButton()
 		{
 		}
+		[Description("The image displayed in the button if there is an Image Control in the template whose Source property is template-bound to the ImageButton Source property."), Category("Common Properties")]
 		public ImageSource Source
 		{
 			get { return base.GetValue(SourceProperty) as ImageSource; }

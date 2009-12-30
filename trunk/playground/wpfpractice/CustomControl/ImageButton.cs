7200f784db45cab94ae2846a095d8d471f00eb54
xf10036 <xf10036@7d216c18-c956-11de-a675-93be1f20d5c2>
2009-12-30 00:33:37 +0000

    copy of default button template

---
+++ trunk/playground/wpfpractice/CustomControl/ImageButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CustomControl
+{
+	public class ImageButton : Button
+	{
+		public ImageButton()
+		{
+		}
+		public ImageSource Source
+		{
+			get { return base.GetValue(SourceProperty) as ImageSource; }
+			set { base.SetValue(SourceProperty, value); }
+		}
+		public static readonly DependencyProperty SourceProperty =
+			DependencyProperty.Register("Source", typeof(ImageSource), typeof(ImageButton), null);
+	}
+}
